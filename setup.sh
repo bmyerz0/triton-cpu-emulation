14d432b55ed2f040ab57b3b2caa3457ee20d2979
Brandon Myers <brandonmyers@microsoft.com>
2023-06-14 16:41:25 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-    FROM  mcr.microsoft.com/devcontainers/universal:2
-
-RUN pip install -r requirements.txt
-RUN pip install torch --index-url https://download.pytorch.org/whl/cpu
-RUN pip uninstall triton
-RUN cd triton/python && pip install --verbose -e .
+pip install -r requirements.txt
+pip install torch --index-url https://download.pytorch.org/whl/cpu
+pip uninstall triton
+pushd triton/python
+pip install --verbose -e .
+popd
