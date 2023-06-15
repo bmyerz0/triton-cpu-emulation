fc029b8a5cd089e010b71fa2de7f22971fffcd64
Brandon Myers <brandonmyers@microsoft.com>
2023-06-15 16:40:20 -0700

    install from prebuilt whl

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ pip install torch --index-url https://download.pytorch.org/whl/cpu
 pip uninstall -y triton
 git clone https://github.com/bmyerz0/triton.git
 cd triton && git fetch && git checkout dev/bmyerz0/codespaces-support; cd - 
-cd triton/python && pip install --verbose -e .; cd -
+pip install triton-2.1.0-cp310-cp310-linux_x86_64.whl
