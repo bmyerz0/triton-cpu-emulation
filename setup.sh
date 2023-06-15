d59337fee5c969bb36ac3c1bbcbf4ba325053ce6
Brandon Myers <brandonmyers@microsoft.com>
2023-06-14 17:01:15 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 pip install -r requirements.txt
 pip install torch --index-url https://download.pytorch.org/whl/cpu
 pip uninstall -y triton
-git clone https://github.com/openai/triton.git
+git clone https://github.com/bmyerz0/triton.git
+cd triton && git fetch && git checkout dev/bmyerz0/codespaces-support; cd - 
 cd triton/python && pip install --verbose -e .; cd -
