3beae099466bfecf9f03d4506ff6b6fdf4ba7136
Brandon Myers <brandonmyers@microsoft.com>
2023-09-18 16:44:11 -0700

    modify tutorials for legacy interpreter

--- setup-with-triton-whl.sh
+++ setup-with-triton-whl.sh
@@ -2,5 +2,5 @@ pip install -r requirements.txt
 pip install torch --index-url https://download.pytorch.org/whl/cpu
 pip uninstall -y triton
 git clone https://github.com/bmyerz0/triton.git
-cd triton && git fetch && git checkout dev/bmyerz0/codespaces-support; cd - 
+cd triton && git fetch && git checkout dev/bmyerz0/codespaces-support-with-tutorials; cd - 
 pip install triton-2.1.0-cp310-cp310-linux_x86_64.whl
